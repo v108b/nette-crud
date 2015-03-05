e46a6d3b074ed990a004273512d719d4a465b038
Vaclav Juchelka <vjuchelka@gmail.com>
2015-03-05 01:17:57 +0100

    default values fix

--- doc/example/CrudPresenter.php
+++ doc/example/CrudPresenter.php
@@ -4,11 +4,29 @@ namespace App\Presenters;
 
 class CrudPresenter extends BasePresenter {
 
-	public function actionInsertTableRow($tableName)
+	public function actionInsertTableRow($tableName, $defaults = null)
 	{
 		$this->template->tableName = $tableName;
 		$form = $this->getCrudExtenstion()->getForms()->buildInsertForm($tableName);
+		if ($defaults) {
+			$form->setDefaults(json_decode($defaults, true));
+		}
 		$this->addComponent($form, 'crudInsertRowForm');
+
+		$form->onSuccess[] = function() use ($tableName) {
+			$this->redirect('listTable', $tableName);
+		};
+	}
+
+	public function actionUpdateTableRow($tableName, $id, $defaults = null)
+	{
+		$this->template->tableName = $tableName;
+		$this->template->id = $id;
+		$form = $this->getCrudExtenstion()->getForms()->buildUpdateForm($tableName, json_decode($id, true));
+		if ($defaults) {
+			$form->setDefaults(json_decode($defaults, true));
+		}
+		$this->addComponent($form, 'crudUpdateRowForm');
 
 		$form->onSuccess[] = function() use ($tableName) {
 			$this->redirect('listTable', $tableName);
@@ -21,18 +39,6 @@ class CrudPresenter extends BasePresenter {
 		$this->template->id = $id;
 		$form = $this->getCrudExtenstion()->getForms()->buildDeleteForm($tableName, json_decode($id, true));
 		$this->addComponent($form, 'crudDeleteRowForm');
-
-		$form->onSuccess[] = function() use ($tableName) {
-			$this->redirect('listTable', $tableName);
-		};
-	}
-
-	public function actionUpdateTableRow($tableName, $id)
-	{
-		$this->template->tableName = $tableName;
-		$this->template->id = $id;
-		$form = $this->getCrudExtenstion()->getForms()->buildUpdateForm($tableName, json_decode($id, true));
-		$this->addComponent($form, 'crudUpdateRowForm');
 
 		$form->onSuccess[] = function() use ($tableName) {
 			$this->redirect('listTable', $tableName);
